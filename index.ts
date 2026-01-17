1bafe79230db03f7ebea9ac5493ce74664a790ad
msyawali88 <syawali2003@gmail.com>
2026-01-18 00:59:08 +0700

    fix(cors): correct IP address in allowed origins list

--- index.ts
+++ index.ts
@@ -13,7 +13,7 @@ const allowedOrigins = [
   "http://localhost:3000",
   "http://localhost:3001",
   "http://127.0.0.1:3000",
-  "http://192.168.1.83:3000",
+  "http://192.168.1.8:3000",
   process.env.FRONTEND_URL || "http://localhost:3000",
 ];
 
